ad66f2a553f1d037cb05e33d0ea58bf4a7960d84
William Fisher <williamsykesfisher@gmail.com>
2015-09-25 11:16:10 +0100

    Update wed.h

--- wed.h
+++ wed.h
@@ -7,7 +7,7 @@ typedef struct Position {
 } Position;
 
 Position make_position(int, int);
-Position get_new_positio(Position, int);
+Position get_new_position(Position, int);
 Position compute_change(Position, Position);
 int out_of_bounds(Position, int, int);
 void draw_coords(WINDOW *, Position);
