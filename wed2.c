b096a78ac35038e939059294be348b74029619d5
FilWisher <williamfisher@hotmail.co.uk>
2015-09-27 23:02:05 +0100

    adds todos and size_of_string function to check size of char *s

--- wed2.c
+++ wed2.c
@@ -1,21 +1,42 @@
+/*
+ * TODO: Read/write files (and wtf is fflush)
+ * TODO: Define key bindings in config.h
+ * TODO: Define movement and edit functions
+ * TODO: Check keypresses against keybindings
+ * TODO: Draw text buffer into screen
+ * TODO: Edit buffer via pointer to text[]
+ * TODO: Write unit tests to check functionality
+ */
+
 #include <stdio.h>
 #include <curses.h>
 
 #define WINDOW_WIDTH 40
 #define WINDOW_HEIGHT 12
 
+/* typedefs */
 typedef struct {
   int col;
   int line;
 } Position;
 
-static Position cur_pos;
+static Position cur_pos; /* cursor position */
 
+size_t size_of_string(char *);
 int l_keypress(void);
 void get_key_press(void);
 void draw_screen(WINDOW *);
 void l_redraw_screen(WINDOW *);
 void kill_screen(WINDOW *);
+
+size_t
+size_of_string(char *string)
+{
+  int size = 0;
+  while(*(string+size) != '\0')
+    size++;
+  return (size_t)size;
+}
 
 int
 l_keypress(void)
@@ -32,12 +53,13 @@ l_keypress(void)
 
 void
 draw_screen(WINDOW *win)
+
 {
   initscr();
   start_color();
   init_pair(1, COLOR_WHITE, COLOR_BLUE);
   win = newwin(WINDOW_HEIGHT, WINDOW_WIDTH, 0, 0);
-  wmove(win, 0, 5);
+  wmove(win, cur_pos.line, cur_pos.col);
   wbkgd(win, COLOR_PAIR(1));
 }
 
@@ -45,6 +67,7 @@ void
 l_redraw_screen(WINDOW *win)
 {
   win = newwin(WINDOW_HEIGHT, WINDOW_WIDTH, 0, 0);
+  wmove(win, cur_pos.line, cur_pos.col);
   wbkgd(win, COLOR_PAIR(1));
   wrefresh(win);
 }
@@ -61,7 +84,9 @@ main(void)
 {
   WINDOW *win;
   int ch;
- 
+
+  cur_pos.line = 2;
+  cur_pos.col = 2;
   keypad(stdscr, TRUE); 
   noecho();
   draw_screen(win); 
